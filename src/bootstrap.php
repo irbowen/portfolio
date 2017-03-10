66c007d67743fff203e10ebc1d535b873331e977
Isaac Bowen <irbowen@umich.edu>
2017-03-10 00:58:27 -0500

    Adding whoops for error handling

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,7 +1,8 @@
-<?php
-declare(strict_types = 1);
+<?php declare(strict_types = 1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
 
 define('API_ROUTES', ['blog']);
 
@@ -9,30 +10,43 @@ $klein = new \Klein\Klein();
 
 $klein->respond(function ($request, $response, $service, $app) use ($klein) {
 
-  $app->register('smarty', function() {
-    /* This should be passed along with every request */
-    $smarty = new Smarty();
-    $smarty->setTemplateDir(__DIR__ . '/templates/templates/');
-    $smarty->setCompileDir(__DIR__ . '/templates/templates_c/');
-    /* End generic Smarty setup */
-    return $smarty;
-  });
+    $app->register('smarty', function() {
+        $smarty = new Smarty();
+        $smarty->setTemplateDir(__DIR__ . '/templates/templates/');
+        $smarty->setCompileDir(__DIR__ . '/templates/templates_c/');
+        return $smarty;
+    });
+
+    $environment = 'dev';
+    $app->register('whoops', function() {
+        $whoops =  new \Whoops\Run;
+        if ($environment !== 'prod') {
+            $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+        } else {
+            $whoops->pushHandler(function($e){
+                echo 'Todo: Friendly error page and send an email to the developer';
+            });
+        }
+        return $whoops;
+    });
+    $app->whoops->register();
+
 });
 
 $klein->respond('GET', '/',
-  function ($request, $response, $service, $app) {
-    $app->smarty->assign('title', 'Isaac Bowen');
-    $app->smarty->assign('image_dir', 'images');
-    $app->smarty->display('index/index.tpl');
-  });
+    function ($request, $response, $service, $app) {
+        $app->smarty->assign('title', 'Isaac Bowen');
+        $app->smarty->assign('image_dir', 'images');
+        $app->smarty->display('index/index.tpl');
+    });
 
 $klein->respond('GET', '/eecs485/',
-  function ($request, $response, $service, $app) {
-    $app->smarty->display('eecs485.tpl');
-  });
+    function ($request, $response, $service, $app) {
+        $app->smarty->display('eecs485.tpl');
+    });
 
 foreach (API_ROUTES as $controller) {
-  $klein->with("/$controller", "../src/controllers/$controller.php");
+    $klein->with("/$controller", "../src/controllers/$controller.php");
 }
 
 $klein->dispatch();
